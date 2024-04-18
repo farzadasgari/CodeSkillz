b7a0794e94065e1031220094e2fcf42ee4d6b494
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-04-19 00:20:39 +0330

    Update url patterns for static and media files

--- CodeSkillz/urls.py
+++ CodeSkillz/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'', include('center.urls')),
     re_path(r'^panel/', include('panel.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
