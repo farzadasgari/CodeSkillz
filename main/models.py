7bdf050a2f52404dfeb9a97b60ada07ce8bc9281
ZarRza <160187613+ZarRza@users.noreply.github.com>
2024-04-18 23:52:17 +0330

    Create main\models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
+class Main(models.Model):
+    class Meta:
+        verbose_name_plural = 'main_detail'
+
+    website_title = models.CharField(max_length=30, blank=True)
+    browser_title = models.CharField(max_length=30, blank=True)
+    panel_title = models.CharField(max_length=30, blank=True)
+    logo_image = models.CharField(max_length=30, blank=True)
+    logo_image_url = models.TextField(blank=True)
+    favicon = models.CharField(max_length=30, blank=True)
+    favicon_url = models.TextField(blank=True)
+    copyright = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.website_title
