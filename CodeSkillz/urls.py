6ea4ef69776f97ef50902b16db8e0d2a14123616
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-04-19 18:26:10 +0330

    Footer was added to the UI section

--- CodeSkillz/urls.py
+++ CodeSkillz/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    re_path(r'^panel/', include('panel.urls')),
     re_path(r'^admin/', admin.site.urls),
     re_path(r'', include('center.urls')),
-    re_path(r'^panel/', include('panel.urls'))
 ]
 
 if settings.DEBUG:
