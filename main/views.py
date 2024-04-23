b3252d6741e4e6469dd4eae9cf0e819e955bd9e9
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-04-23 23:47:47 +0330

    Initializing panel configs page

--- main/views.py
+++ main/views.py
@@ -1,1 +1,6 @@
+from django.shortcuts import render
+
+
+def panel_configs(request):
+    return render(request, 'panel/main/configs.html')
 
--- panel/urls.py
+++ panel/urls.py
@@ -1,6 +1,9 @@
 from django.urls import re_path
 from .views import panel
+from main.views import panel_configs
+
 
 urlpatterns = [
-    re_path(r'', panel, name='panel')
+    re_path(r'', panel, name='panel'),
+    re_path(r'^configs/$', panel_configs, name='panel_configs'),
 ]
