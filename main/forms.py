6a19b1b00c2e044ca1df5f7ca7ed56a4bb3dfab5
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-04-24 17:47:13 +0330

    The form template was added in the configs template

--- main/forms.py
+++ main/forms.py
@@ -5,4 +5,10 @@ from .models import Main
 class MainForm(forms.ModelForm):
     class Meta:
         model = Main
-        fields = '__all__'
+        exclude = ['logo_image', 'logo_image_url', 'light_logo', 'light_logo_url', 'dark_logo', 'dark_logo_url',
+                   'favicon', 'favicon_url']
+
+    logo_image = forms.ImageField()
+    light_logo = forms.ImageField()
+    dark_logo = forms.ImageField()
+    favicon = forms.ImageField()
