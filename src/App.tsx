1222efa91f7910bcdda9eb42a5f3e54ea0905fc6
Farzad Asgari <khufarzadasgari@gmail.com>
2025-10-28 21:00:35 +0330

    initializing home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <h1 className="text-rose-500 text-3xl font-bold text-center">
-        CodeSkillz
-    </h1>
-  )
+    return (
+        <BrowserRouter>
+            <div className="flex flex-col min-h-screen">
+                <main className="flex-1">
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <h1 className="text-center text-primary text-3xl font-bold">
+            CodeSkillz
+        </h1>
+    )
+}
+
+export default Home;
