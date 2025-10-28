43a43253dde030bdb572276d8c82bc561e1d6297
Farzad Asgari <khufarzadasgari@gmail.com>
2025-10-28 23:54:51 +0330

    modify eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-    { ignores: ["dist"] },
+    { ignores: ["dist", "node_modules", "tailwind.config.ts", "src/components/ui"] },
     {
         extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ["**/*.{ts,tsx}"],
