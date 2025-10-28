8eb2bd9b0825ef0d73a2c0ef6d83906809421f3c
Farzad Asgari <khufarzadasgari@gmail.com>
2025-10-28 21:32:16 +0330

    debug deployment

--- eslint.config.js
+++ eslint.config.js
@@ -7,20 +7,20 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
     { ignores: ["dist"] },
     {
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
-      files: ["**/*.{ts,tsx}"],
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-      },
-      plugins: {
-        "react-hooks": reactHooks,
-        "react-refresh": reactRefresh,
-      },
-      rules: {
-        ...reactHooks.configs.recommended.rules,
-        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-        "@typescript-eslint/no-unused-vars": "off",
-      },
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+            "@typescript-eslint/no-unused-vars": "off",
+        },
     },
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
