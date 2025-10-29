ba00c890525bc1e478db631747f27c99b5bdfc1b
Farzad Asgari <khufarzadasgari@gmail.com>
2025-10-29 14:03:43 +0330

    initializing header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Header from "./components/master/Header";
 
 function App() {
     return (
         <BrowserRouter basename="/CodeSkillz">
             <div className="flex flex-col min-h-screen">
+                <Header />
                 <main className="flex-1">
                     <Routes>
                         <Route path="/" element={<Home />} />
---
+++ src/components/master/Header.tsx
@@ -0,0 +1,9 @@
+import {useTranslation} from "react-i18next";
+const Header = () => {
+    const { t } = useTranslation();
+    return (
+        <>Header</>
+    )
+}
+
+export default Header;
