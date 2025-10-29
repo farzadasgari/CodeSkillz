088f8f0a08fdae57482a2cede665d7552a665a68
Farzad Asgari <khufarzadasgari@gmail.com>
2025-10-29 14:31:13 +0330

    add langs for header section

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -4,13 +4,17 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 import homeEN from '@/locales/en/home.json';
 import homeFA from '@/locales/fa/home.json';
+import masterEN from '@/locales/en/master.json';
+import masterFA from '@/locales/fa/master.json';
 
 const resources = {
     en: {
         home: homeEN,
+        master: masterEN
     },
     fa: {
         home: homeFA,
+        master: masterFA
     },
 };
 
