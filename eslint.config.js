6c827248c88bfb8275c073608e94f72ed2822df1
Farzad Asgari <khufarzadasgari@gmail.com>
2025-12-13 13:15:34 +0330

    update lint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,31 +5,22 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-    {
-        ignores: [
-            "dist",
-            "node_modules",
-            "tailwind.config.ts",
-            "src/components/ui",
-            "src/contexts",
-            "src/i18n"
-        ]
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-    {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
-        files: ["**/*.{ts,tsx}"],
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
-        },
-        plugins: {
-            "react-hooks": reactHooks,
-            "react-refresh": reactRefresh,
-        },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
-            "react-refresh/only-export-components": ["warn", {allowConstantExport: true}],
-            "@typescript-eslint/no-unused-vars": "off",
-        },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 );
